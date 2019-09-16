272cb69b2795731b551debe6000632ea4cb99ee9
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2019-09-16 13:19:21 +0530

    updated version

--- Project/mysite/settings.py
+++ Project/mysite/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h&qtmbv=7w-prp9w=&mdkxf@%&epoa@jl_)metg9#++xg(7r0u'
+# SECRET_KEY = 'h&qtmbv=7w-prp9w=&mdkxf@%&epoa@jl_)metg9#++xg(7r0u'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'h&qtmbv=7w-prp9w=&mdkxf@%&epoa@jl_)metg9#++xg(7r0u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tvish.herokuapp.com']
+ALLOWED_HOSTS = ['tvish.herokuapp.com','localhost']
 
 
 # Application definition
@@ -177,15 +178,21 @@ CMS_PLACEHOLDER_CONF = {}
 
 DATABASES = {
     'default': {
+        'CONN_MAX_AGE': 0,
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'project.db'),
+        'HOST': 'tvish.herokuapp.com',
+        'NAME': 'project.db',
+        'PASSWORD': '',
+        'PORT': '',
+        'USER': ''
+
     }
 }
 
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-DATABASES['default']['CONN_MAX_AGE'] = 500
+# import dj_database_url
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
+# DATABASES['default']['CONN_MAX_AGE'] = 500
 
 MIGRATION_MODULES = {
     
@@ -198,12 +205,12 @@ THUMBNAIL_PROCESSORS = (
     'easy_thumbnails.processors.filters'
 )
 
-CORS_REPLACE_HTTPS_REFERER      = True
-HOST_SCHEME                     = "https://"
-SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT             = True
-SESSION_COOKIE_SECURE           = True
-CSRF_COOKIE_SECURE              = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
-SECURE_HSTS_SECONDS             = 1000000
-SECURE_FRAME_DENY               = True
+# CORS_REPLACE_HTTPS_REFERER      = True
+# HOST_SCHEME                     = "https://"
+# SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT             = True
+# SESSION_COOKIE_SECURE           = True
+# CSRF_COOKIE_SECURE              = True
+# SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+# SECURE_HSTS_SECONDS             = 1000000
+# SECURE_FRAME_DENY               = True
